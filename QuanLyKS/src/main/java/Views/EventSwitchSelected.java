e58de11c8e24e37d9656bff7ca0e06fa5c0a1e40
quanhmph19157 <109107690+quanhmph19157@users.noreply.github.com>
2022-08-19 03:45:37 +0700

    c

--- QuanLyKS/src/main/java/Views/EventSwitchSelected.java
+++
@@ -1,6 +0,0 @@
-package Views;
-
-public interface EventSwitchSelected {
-	public void onSelected (boolean selected);
-
-}
--- QuanLyKS/src/main/java/utils/File.java
+++
@@ -1,7 +0,0 @@
-package utils;
-
-public class File {
-	public static String fileGhiNhoMatKhau() {
-		return "text.txt";
-	}
-}
